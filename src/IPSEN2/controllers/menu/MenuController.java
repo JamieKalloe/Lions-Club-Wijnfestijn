41dfb548531639a0bdc79cb907d2012ed40b24cc
Phillywonka <philip_wong@live.nl>
2015-09-18 23:24:53 +0200

    Event Handlers aan minimize en close button toegevoegd

--- src/IPSEN2/controllers/menu/MenuController.java
+++ src/IPSEN2/controllers/menu/MenuController.java
@@ -1,9 +1,9 @@
 package IPSEN2.controllers.menu;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import java.net.URL;
@@ -16,7 +16,7 @@ public class MenuController implements Initializable{
     private Stage primaryStage;
 
     public void handleMousePressed(MouseEvent event) throws Exception{
-        primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        getPrimaryStage(event);
         xOffset = primaryStage.getX() - event.getScreenX();
         yOffset = primaryStage.getY() - event.getScreenY();
     }
@@ -25,6 +25,19 @@ public class MenuController implements Initializable{
     public void handleMouseDragged(MouseEvent event) throws Exception{
         primaryStage.setX(event.getScreenX() + xOffset);
         primaryStage.setY(event.getScreenY() + yOffset);
+    }
+
+    public void handleMinimizeButton(MouseEvent event) {
+        getPrimaryStage(event).toBack();
+    }
+    @FXML
+    public void handleCloseButton(MouseEvent event) throws Exception {
+            getPrimaryStage(event).close();
+    }
+
+    public Stage getPrimaryStage(Event event) {
+        primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        return primaryStage;
     }
 
     @Override
