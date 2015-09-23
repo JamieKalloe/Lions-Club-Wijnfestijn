386931e9845a2006c8d529abf3d08a9262f33b4b
JamieKalloe <Jamie@Kalloe.net>
2015-09-23 14:45:31 +0100

    fixed order bestelling reference

--- src/IPSEN2/Main.java
+++ src/IPSEN2/Main.java
@@ -45,7 +45,7 @@ public class Main extends Application{
     }
 
     public static void main(String args[]){
-        new GuestController();
+//        new GuestController();
         launch(args);
     }
 
