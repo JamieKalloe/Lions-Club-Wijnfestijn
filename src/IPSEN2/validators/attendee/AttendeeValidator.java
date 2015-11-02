b1c3f52f625d09d2ff99ed91d80eba79d46aebe4
Thomas Neuteboom <neuteboom.thomas@gmail.com>
2015-11-02 13:20:04 +0100

    Added AttendeeValidator.

--- src/IPSEN2/validators/attendee/AttendeeValidator.java
+++ src/IPSEN2/validators/attendee/AttendeeValidator.java
@@ -12,9 +12,8 @@ public class AttendeeValidator extends Validator implements Validatable {
 
     public boolean validate(HashMap data)
     {
-        return Integer.parseInt(data.get("guestId").toString()) != -1
-                && Integer.parseInt(data.get("eventId").toString()) != -1
-                && Integer.parseInt(data.get("orderStatusId").toString()) != -1;
+        return Integer.parseInt(data.get("guestID").toString()) != -1
+                && Integer.parseInt(data.get("eventID").toString()) != -1;
     }
 
 }
