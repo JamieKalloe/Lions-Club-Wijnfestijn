2ab6ffff4bb057aa0e29e1ea5767b54cca7cc2b2
Phillywonka <philip_wong@live.nl>
2015-11-04 02:09:18 +0100

    fixed EditEventController

--- src/IPSEN2/controllers/event/EditEventController.java
+++ src/IPSEN2/controllers/event/EditEventController.java
@@ -1,6 +1,7 @@
 package IPSEN2.controllers.event;
 
 import IPSEN2.ContentLoader;
+import IPSEN2.models.event.Event;
 import IPSEN2.services.event.EventService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -27,8 +28,9 @@ public class EditEventController extends ContentLoader implements Initializable 
     private LocalDate date;
 
     private HashMap data;
-    private EventService service;
+    private EventService eventService;
     private int selectedEventId;
+    private Event event;
 
     @FXML private Pane submitButton, cancelButton;
     @FXML private TextField eventNameTextField, cityTextField,
@@ -60,6 +62,7 @@ public class EditEventController extends ContentLoader implements Initializable 
         }
 
         data = new HashMap();
+        data.put("addressID" , event.getAddress().getAddressID());
         data.put("name", name);
         data.put("zipCode", zipCode);
         data.put("street", address);
@@ -69,18 +72,23 @@ public class EditEventController extends ContentLoader implements Initializable 
         data.put("startDate", dateFormatter.format(date));
         data.put("endDate", dateFormatter.format(date));
 
-        eventId = service.add(data);
+        eventService.edit(selectedEventId, event.getAddress().getAddressID(), data);
 
-        if (eventId != -1)
-        {
-            addContent(EVENTS);
-        }
+        addContent(EVENTS);
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        submitButton.setOnMouseClicked(event -> handleSubmitButton());
-        cancelButton.setOnMouseClicked(event -> handleCancelButton());
-        service = new EventService();
+        event = new EventService().find(selectedEventId);
+        eventNameTextField.setText(event.getName());
+        cityTextField.setText(event.getAddress().getCity());
+        addressTextField.setText(event.getAddress().getStreet());
+        houseNumberTextField.setText(event.getAddress().getHouseNumber());
+        zipCodeTextField.setText(event.getAddress().getZipCode());
+        datePicker.setValue(new java.sql.Date(event.getStartDate().getTime() ).toLocalDate());
+        submitButton.setOnMouseClicked(mouseEvent -> handleSubmitButton());
+        cancelButton.setOnMouseClicked(mouseEvent -> handleCancelButton());
+        eventService = new EventService();
     }
 }
