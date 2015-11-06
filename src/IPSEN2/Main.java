42315837a3812e6d19a5527a4697cc938712b0ac
Guus <mij@guusseldenthuis.nl>
2015-11-06 15:29:50 +0100

    Removed comments.

--- src/IPSEN2/Main.java
+++ src/IPSEN2/Main.java
@@ -25,25 +25,6 @@ public class Main extends Application {
 
 
     public static void main(String args[]){
-        //EVENT TESTING: Works
-        /*EventService service = new EventService();
-        HashMap data = new HashMap();
-        data.put("name", "Wijnproeverij deze shit");
-        data.put("startDate", new Date());
-        data.put("endDate", new Date());
-        data.put("zipCode", "1354RT");
-        data.put("street", "Wat is deze laan");
-        data.put("houseNumber", "4");
-        data.put("country", "Nederland");
-        data.put("city", "Gekke dorp");
-
-        service.add(data);*/
-//        try {
-//            new InvoiceGenerator().generate("bin/IPSEN2.invoice.pdf");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
         launch(args);
     }
 
